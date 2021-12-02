db19727d7b391d27b88e77039000327196b59cae
Vineeth1129 <91303321+Vineeth1129@users.noreply.github.com>
2021-12-02 11:07:29 +0530

    Add files via upload

---
+++ Vineeth1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author STUDENT
+ */
+public class Vineeth1 {
+    public static void main(String args[])
+    {
+        int num1=5,num2=10,sum;
+        sum=num1+num2;
+        System.out.println("sum of two numbers:"+sum);
+    }
+}
